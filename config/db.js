c3e88e4865ea4f3195e528df754aac6c34931b60
Ragul A <153734514+Ragul3504@users.noreply.github.com>
2025-02-21 12:57:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,13 @@
-
-// db.js
 const { Pool } = require("pg");
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config(); // Ensure this is loaded to read the .env file
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD),
-  port: Number(process.env.DB_PORT),
+  password: String(process.env.DB_PASSWORD), // Ensure password is a string
+  port: Number(process.env.DB_PORT), // Ensure port is a n umber 
 });
-module.exports = pool; 
+
+module.exports = pool;
