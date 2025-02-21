cea97a1078f542d0af1065492d88f4b7730a548a
Ragul A <153734514+Ragul3504@users.noreply.github.com>
2025-02-21 11:27:14 +0530

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,11 +1,9 @@
 // supabase.js
-const dotenv = require("dotenv");
-dotenv.config();
 const { createClient } = require("@supabase/supabase-js");
 
+dotenv.config();
 const supabaseUrl = "https://zqphedsadmwotmtpcqgz.supabase.co";
-
 const supabaseKey = process.env.SUPABASE_API_KEY;
-
 const supabase = createClient(supabaseUrl, supabaseKey);
+
 module.exports = supabase;
