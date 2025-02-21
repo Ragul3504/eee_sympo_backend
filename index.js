6cee286efdf0a76dd1b0aaaf267a0489721cf69a
Ragul A <153734514+Ragul3504@users.noreply.github.com>
2025-02-21 13:03:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const registrationRoutes = require("./routes/registrationRoutes");
 
-dotenv.config();
+dotenv.config();  // Load environment variables from .env file
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Routes
 app.use("/api/register", registrationRoutes);
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
